51c0b56fd5f66bb77426e27f5073a464706fa492
JihyeKim <jihyekim830@gmail.com>
2026-01-12 17:09:30 +0900

    chore: 프리텐다드 폰트 설정 및 적용 (#6)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata } from 'next'
+import localFont from 'next/font/local'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'studigo',
-  description: '함께 성장하는 즐거움, studigo',
+  title: 'StudiGo',
+  description: '함께 성장하는 즐거움, StudiGo',
 }
+
+const pretendard = localFont({
+  src: './PretendardVariable.woff2',
+  display: 'swap',
+  variable: '--font-pretendard',
+})
 
 export default function RootLayout({
   children,
@@ -12,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="ko">
-      <body className="antialiased">{children}</body>
+    <html lang="ko" className={`${pretendard.variable} antialiased`}>
+      <body>{children}</body>
     </html>
   )
 }
