3a12bb5b790e0209230be12c60aaa7eb41f6d1aa
JihyeKim <jihyekim830@gmail.com>
2026-01-17 23:19:05 +0900

    rename: 버튼 컴포넌트 위치 이동 (#34)

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,7 +2,7 @@ import { Toaster } from '@/shared/ui/Toaster'
 import Footer from '@/shared/ui/footer/Footer'
 import { Modal, ModalClose } from '@/shared/ui/Modal'
 import Header from '@/shared/ui/header/Header'
-import { Button, ButtonVariants } from '@/shared/ui/button/Button'
+import { Button, ButtonVariants } from '@/shared/ui/Button'
 import { Input } from '@/shared/ui/input/Input'
 
 export {
